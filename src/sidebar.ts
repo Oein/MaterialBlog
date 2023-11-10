835ec27ad4d00f47287b26ee723758987648e200
Oein <oein0219@gmail.com>
2023-11-10 16:56:15 +0900

    add sidebar

---
+++ src/sidebar.ts
@@ -0,0 +1,44 @@
+(() => {
+  let sidebar = document.getElementById("sidebar");
+  if (sidebar == null) throw new Error("DOM : Sidebar not found");
+
+  let sidebarItemsDOMLIST = document.querySelectorAll("#sidebar > a");
+  if (sidebarItemsDOMLIST.length == 0)
+    throw new Error("DOM : Sidebar items not found");
+
+  let sidebarItems = Array.from(sidebarItemsDOMLIST)
+    .filter((x) => x.getAttribute("nomove") != "")
+    .sort((a, b) => {
+      let ap = a.getAttribute("path");
+      let bp = b.getAttribute("path");
+      if (!ap || !bp) throw new Error("DOM : Sidebar item path not found");
+
+      // compare which path is deeper
+      let aDepth = ap.split("/").length;
+      let bDepth = bp.split("/").length;
+      if (aDepth == bDepth) {
+        // if depth is same, compare which path is longer
+        return bp.length - ap.length;
+      }
+      return aDepth - bDepth;
+    });
+
+  sidebarItems.forEach((item) => {
+    item.addEventListener("click", () => {
+      let path = item.getAttribute("path");
+      if (!path) throw new Error("DOM : Sidebar item path not found");
+      (window as any).moveTo(path);
+    });
+  });
+
+  for (let i = 0; i < sidebarItems.length; i++) {
+    let item = sidebarItems[i];
+    let path = item.getAttribute("path");
+    if (!path) throw new Error("DOM : Sidebar item path not found");
+
+    if (location.pathname.startsWith(path)) {
+      item.setAttribute("enabled", "");
+      break;
+    }
+  }
+})();
