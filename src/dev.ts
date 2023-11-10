7d433a29c104f627a59ad359e3abe05a988fce3d
Oein <oein0219@gmail.com>
2023-11-10 16:56:32 +0900

    dev file

---
+++ src/dev.ts
@@ -0,0 +1,12 @@
+const dev = () => {
+  document.body.setAttribute(
+    "style",
+    `--img: url('https://t1.daumcdn.net/tistory_admin/static/manage/images/r3/default_L.png');`
+  );
+
+  document.body.innerHTML = document.body.innerHTML
+    .replace(/\[##_title_##\]/g, "DEV TISTORY")
+    .replace(/\[##_desc_##\]/g, "BLOG DESC");
+};
+
+if (location.hostname == "localhost") dev();
