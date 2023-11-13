2575351ccbc0a85a38fb9799ecbd8ea8e931584a
Oein <oein0219@gmail.com>
2023-11-13 20:39:18 +0900

    add css applyer by body id

---
+++ src/bodyIDApplyer.ts
@@ -0,0 +1,33 @@
+try {
+  let id = document.body.id;
+
+  switch (id) {
+    case "tt-body-index":
+      [...document.querySelectorAll(".viewOnly_home")].forEach((e) => {
+        e.classList.remove("viewOnly_home");
+      });
+      break;
+    case "tt-body-page":
+      document.getElementById("mayrowview")?.classList.remove("rowview");
+      [...document.querySelectorAll(".notOnPage")].forEach((e) => {
+        e.remove();
+      });
+      [...document.querySelectorAll(".onPage")].forEach((e) => {
+        e.classList.remove("onPage");
+      });
+      let mvh = document.getElementById("move2here_1");
+      let mvo = document.getElementById("moveOnLoad_1");
+      if (mvh && mvo) {
+        mvh.outerHTML = mvo.outerHTML;
+        mvo.remove();
+      }
+      break;
+    case "tt-body-search":
+      [...document.querySelectorAll(".viewOnly_search")].forEach((e) => {
+        e.classList.remove("viewOnly_search");
+      });
+      break;
+  }
+} catch (e) {
+  console.error(e);
+}
